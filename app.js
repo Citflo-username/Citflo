07af374d38eec2283f823ee5531b1ec1c26bfb0d
DwightTheSower <cdhouweling@hotmail.com>
2025-05-17 19:44:16 -0400

    First layout of home page

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
-	res.send('Welcome to laboratoire Citflo, the home for research from Professor Dwight Houweling. Great things are coming...');
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+	res.render('main'); // Looks for views/main.ejs
 });
 
 app.listen(port, () => {
-	console.log(`Server running at local host: ${port}`);
+	console.log(`Server running at http://localhost:${port}`);
 });
