caad5b3476f38482b90b0b5e6bc54abc44db60b8
DwightTheSower <cdhouweling@hotmail.com>
2025-05-17 18:52:23 -0400

    testing the ssh connection to github

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-	res.send('Hello world');
+	res.send('Welcome to laboratoire Citflo, the home for research from Professor Dwight Houweling');
 });
 
 app.listen(port, () => {
